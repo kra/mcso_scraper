f5c17e7e201b41f4fb8230b364ead933305cf832
Karl Anderson <kra@monkey.org>
2012-02-13 18:50:01 -0800

    relative data finding

--- mcso/settings.py
+++ mcso/settings.py
@@ -1,3 +1,5 @@
+import os
+
 BOT_NAME = 'mcso'
 BOT_VERSION = '1.0'
 
@@ -8,7 +10,7 @@ DEFAULT_ITEM_CLASS = 'mcso.items.McsoItem'
 ITEM_PIPELINES = ['mcso.pipelines.McsoPipeline']
 # asp will forbid form submission unless we spoof a human user-agent
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_8) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.220 Safari/535.1'
-DATA_DIRNAME = '../data'
+DATA_DIRNAME = '/'.join((os.path.dirname(__file__), '../data'))
 SQLITE_FILENAME = '/'.join([DATA_DIRNAME, 'db'])
 MUGSHOT_DIRNAME = '/'.join([DATA_DIRNAME, 'mugshots'])
 #BOOKINGS_TO_DOWNLOAD = 'IN_CUSTODY'
