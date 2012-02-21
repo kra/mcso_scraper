2d8d8cb2d0094750d0f88ee08d94f182670cd509
Karl Anderson <kra@monkey.org>
2012-02-20 22:46:33 -0800

    __file__ abspath

--- mcso/settings.py
+++ mcso/settings.py
@@ -11,7 +11,7 @@ DEFAULT_ITEM_CLASS = 'mcso.items.McsoItem'
 ITEM_PIPELINES = ['mcso.pipelines.McsoPipeline']
 # asp will forbid form submission unless we spoof a human user-agent
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5_8) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/13.0.782.220 Safari/535.1'
-DATA_DIRNAME = '/'.join((os.path.dirname(__file__), '../data'))
+DATA_DIRNAME = '/'.join((os.path.dirname(os.path.abspath(__file__)), '../data'))
 SQLITE_FILENAME = '/'.join([DATA_DIRNAME, 'db'])
 MUGSHOT_DIRNAME = '/'.join([DATA_DIRNAME, 'mugshots'])
 #BOOKINGS_TO_DOWNLOAD = 'IN_CUSTODY'
