1a89f09fa66c157dd17c35ea7c8a82fd983746da
Karl Anderson <kra@monkey.org>
2012-03-18 19:46:13 -0700

    rudimentary navigation

--- view/static/booking_index.js
+++ view/static/booking_index.js
@@ -5,30 +5,27 @@ $(document).ready(function() {
 
     var booking_index_href = '../data/booking_index'
     booking_index_href += '?_=' + Number(new Date());
-    // XXX we don't need to getJSON anymore
-    $.getJSON(booking_index_href, function(index_objs) {
-        $('#booking_table').dataTable({
-            "bProcessing": true,
-            "bServerSide": true,
-            "sAjaxSource": booking_index_href,
-            "aoColumns": [
-                { "sTitle": "name" },
-                { "sTitle": "age" },
-                { "sTitle": "swisid", "bSortable": false },
-                { "sTitle": "race" },
-                { "sTitle": "gender" },
-                { "sTitle": "parsed_arrestdate" },
-                { "sTitle": "parsed_bookingdate", "bSearchable": true },
-                { "sTitle": "assignedfac" },
-                { "sTitle": "arrestingagency" },
-                { "sTitle": "currentstatus" }],
-        }).columnFilter({
-            "sPlaceHolder": "foot",
-            "aoColumns": [
-                null, null, null, null, null, null,
-                { "type": "date-range"},
-                null, null, null,
-            ]});
-    });
+    $('#booking_table').dataTable({
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": booking_index_href,
+        "aoColumns": [
+            { "sTitle": "name" },
+            { "sTitle": "age" },
+            { "sTitle": "swisid", "bSortable": false },
+            { "sTitle": "race" },
+            { "sTitle": "gender" },
+            { "sTitle": "parsed_arrestdate" },
+            { "sTitle": "parsed_bookingdate", "bSearchable": true },
+            { "sTitle": "assignedfac" },
+            { "sTitle": "arrestingagency" },
+            { "sTitle": "currentstatus" }],
+    }).columnFilter({
+        "sPlaceHolder": "foot",
+        "aoColumns": [
+            null, null, null, null, null, null,
+            { "type": "date-range"},
+            null, null, null,
+        ]});
     
 });    
--- view/static/charge_index.js
+++ view/static/charge_index.js
@@ -5,24 +5,21 @@ $(document).ready(function() {
 
     var charge_index_href = '../data/charge_index'
     charge_index_href += '?_=' + Number(new Date());
-    // XXX we don't need to getJSON anymore
-    $.getJSON(charge_index_href, function(index_objs) {
-        $('#charge_table').dataTable({
-            "bProcessing": true,
-            "bServerSide": true,
-            "sAjaxSource": charge_index_href,
-            "aoColumns": [
-                { "sTitle": "charge" },
-                { "sTitle": "status" },
-                { "sTitle": "parsed_bail"},
-                { "sTitle": "parsed_arrestdate" },
-                { "sTitle": "parsed_bookingdate", "bSearchable": true }]
-        }).columnFilter({
-            "sPlaceHolder": "foot",
-            "aoColumns": [
-                null, null, null, null,
-                { "type": "date-range"}
-            ]});
-    });
+    $('#charge_table').dataTable({
+        "bProcessing": true,
+        "bServerSide": true,
+        "sAjaxSource": charge_index_href,
+        "aoColumns": [
+            { "sTitle": "charge" },
+            { "sTitle": "status" },
+            { "sTitle": "parsed_bail"},
+            { "sTitle": "parsed_arrestdate" },
+            { "sTitle": "parsed_bookingdate", "bSearchable": true }]
+    }).columnFilter({
+        "sPlaceHolder": "foot",
+        "aoColumns": [
+            null, null, null, null,
+            { "type": "date-range"}
+        ]});
     
 });    
