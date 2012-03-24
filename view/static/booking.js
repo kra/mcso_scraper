5195579dcf21d13af08f03981faf7cc16a0cce84
Karl Anderson <kra@monkey.org>
2012-03-23 17:52:18 -0700

    booking styling

--- view/static/booking.js
+++ view/static/booking.js
@@ -22,14 +22,45 @@ $(document).ready(function() {
         return '<dl>' + items.join('') + '</dl>';
     }
 
+    function charge_table_row(charge) {
+        row = '<tr>';
+        row += '<td>' + charge['charge'] + '</td>';
+        row += '<td>' + charge['bail'] + '</td>';
+        row += '<td>' + charge['status'] + '</td>';
+        row += '</tr>';            
+        return row;
+    }
+
+    function charge_table(charges) {
+        ctable = '<div id="charge_table"><table>';
+        ctable += '<tr><td>Charge</td><td>Bail</td><td>Status</td></tr>';
+        $.each(charges, function(index, charge_obj) {
+            ctable += charge_table_row(charge_obj);
+        });
+        ctable += '</table></div>';
+        return ctable;
+    }
+
+    function cases_item(cases) {
+        var out = '';
+        $.each(cases, function(index, case_obj) {
+            header = '<div id="case_header"><ul>';
+            header += '<li>Court Case No. ' + 
+                case_obj['court_case_number'] + '</li>';
+            header += '<li>DA Case No. ' + 
+                case_obj['da_case_number'] + '</li>';
+            header += '<li>Citation No. ' + 
+                case_obj['citation_number'] + '</li>';
+            header += '</div></ul>';
+            out += header + charge_table(case_obj['charges']);
+        });
+        return out;
+    }
+
     function munge_booking_obj(obj) {
-        // munge obj XXX destructively
         obj['mugshot'] = mugshot_img(obj);
         obj['url'] = orig_url(obj['url']);
-        $.each(obj['cases'], function(index, case_obj) {
-            case_obj['charges'] = array_to_dlist(case_obj['charges'], 'charge');
-        });
-        obj['cases'] = array_to_dlist(obj['cases'], 'case');
+        obj['cases'] = cases_item(obj['cases']);
         return obj;
     }
 
