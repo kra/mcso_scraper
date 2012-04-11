78b8dc1629cc2b43fdf665c1736241b514acd508
Karl Anderson <kra@monkey.org>
2012-04-11 07:20:43 -0700

    scrape health page

--- view/server.py
+++ view/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from flask import Flask, g, render_template, helpers, request
+from flask import Flask, g, render_template, helpers, request, abort
 import sqlite3
 import json
 import datetime
@@ -25,7 +25,7 @@ def teardown_request(exception):
     if hasattr(g, 'db'):
         g.db.close()
 
-def rows(cur):
+def row_ds(cur):
     def row_to_dict(row):
         return dict([(k, row[k]) for k in row.keys()])
     return [row_to_dict(row) for row in cur]
@@ -39,7 +39,7 @@ def data_tables_json(items, secho, count_all, count_rows):
          "aaData": items})
 
 def booking_index_rows(cur):
-    index_rows = rows(cur)
+    index_rows = row_ds(cur)
     # XXX should do this in the view, or maybe InmateItem
     for row in index_rows:
         row['name_link'] = (
@@ -48,7 +48,7 @@ def booking_index_rows(cur):
     return index_rows
 
 def charge_index_rows(cur):
-    index_rows = rows(cur)
+    index_rows = row_ds(cur)
     # XXX should do this in the view, or maybe InmateItem
     for row in index_rows:
         # XXX should use InmateItem
@@ -60,7 +60,7 @@ def charge_index_rows(cur):
 @app.route('/data/booking/<bookingid>')
 def data_booking(bookingid):
     # bookingid = urllib.unquote(bookingid)
-    (booking_row,) = rows(g.db.execute(
+    (booking_row,) = row_ds(g.db.execute(
         'SELECT rowid, * FROM bookings WHERE rowid=?', (bookingid,)))
     # XXX should use InmateItem for this
     # booking_row['mugshot_filename'] = (
@@ -68,11 +68,11 @@ def data_booking(bookingid):
     #         booking_row['rowid']))
     booking_row['mugshot_path'] = booking_row['rowid']
     # XXX should be a model for this kind of thing
-    case_rows = rows(g.db.execute(
+    case_rows = row_ds(g.db.execute(
             'SELECT rowid, * FROM cases WHERE booking_id=?',
             [booking_row['rowid']]))
     for case_row in case_rows:
-        charge_rows = rows(g.db.execute(
+        charge_rows = row_ds(g.db.execute(
                 'SELECT rowid, * FROM charges WHERE case_id=?',
                 [case_row['rowid']]))
         case_row['charges'] = charge_rows
@@ -91,7 +91,9 @@ def booking_mugshot(mugshotid):
     path = '/'.join((settings['MUGSHOT_DIRNAME'], path))
     return helpers.send_from_directory(path, mugshotid)
 
-def query_sort(columns):
+def query_sort_args(columns):
+    # single-column sortable only
+    # multi-col with _n and iSortingCols
     # single-column sortable only
     # multi-col with _n and iSortingCols
     try:
@@ -108,10 +110,10 @@ def query_sort(columns):
     except TypeError:
         offset = 0
         length = 10
-    # NOT using sql parameter substitution.  All templated values must be
-    # lookups, inted, or otherwise filtered!
-    return 'ORDER BY %s %s LIMIT %s, %s' % (
-        sort_col, sort_dir, offset, length)
+    return (sort_col, sort_dir, offset, length)
+
+def query_sort(columns):
+    return 'ORDER BY %s %s LIMIT %s, %s' % query_sort_args(columns)
 
 def query_filter(field, start, end):
     """
@@ -213,6 +215,91 @@ def data_charge_index():
             for row in rows]
     return data_tables_json(rows, secho, count_all, count_rows)
 
+@app.route('/data/health/booking_index')
+def data_health_booking_index():
+    period = request.args.get('period')
+    if not period:
+        period = 'week'
+    if period not in ['week', 'day']:
+        abort(400)
+
+    try:
+        secho = int(request.args.get('sEcho'))
+    except TypeError:
+        secho = None
+
+    sort_columns = ['COUNT(rowid)']
+    (sort_col, sort_dir, offset, length) = query_sort_args(sort_columns)
+    if sort_dir != 'ASC':
+        raise NotImplementedError
+
+    def weeks():
+        """
+        Yield week beginning datetimes in descending order,
+        starting with the most recent in the past.
+        """
+        # beginning of today
+        now = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0)
+        # beginning of this week
+        now = now - datetime.timedelta(days=now.weekday())
+        while True:
+            yield now
+            now = now - datetime.timedelta(days=7)
+
+    def days():
+        """
+        Yield day beginning datetimes in descending order,
+        starting with the most recent in the past.
+        """
+        # beginning of today
+        now = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0)
+        while True:
+            yield now
+            now = now - datetime.timedelta(days=1)
+
+    if period == 'week':
+        periods = weeks()
+    else:
+        periods = days()
+
+    to_offset = offset
+    while to_offset:
+        periods.next()
+        to_offset -= 1
+
+    def period_healths(periods):
+        """
+        Yield counts of bookings in preceeding period intervals, starting
+        with the most recent in the past.
+        """
+        (min_date,) = row_ds(
+            g.db.execute('SELECT MIN(updated_on) FROM bookings'))
+        min_date = datetime.datetime.strptime(
+            min_date['MIN(updated_on)'], '%Y-%m-%d %H:%M:%S')
+        end = periods.next()
+        while True:
+            start = periods.next()
+            if end < min_date:
+                raise StopIteration
+            (out,) = row_ds(g.db.execute(
+                'SELECT COUNT(rowid) FROM bookings '
+                'WHERE updated_on>? AND updated_on<?',
+                (start.strftime('%Y-%m-%d %H:%M:%S'),
+                 end.strftime('%Y-%m-%d %H:%M:%S'))))
+            out['period'] = start.strftime('%m/%d/%Y')
+            yield out
+            end = start
+
+    period_healths = period_healths(periods)
+    # We have to spool out the generator to find the total length.  For
+    # effieciency, we could find the earliest date when generating periods
+    # and only spool out that generator.
+    all_rows = [w for w in period_healths]
+    rows = all_rows[:length]
+    rows = [[row['period'], row['COUNT(rowid)']] for row in rows]
+    return data_tables_json(
+        rows, secho, len(all_rows) + offset, len(all_rows) + offset)
+
 @app.route('/booking_index')
 def booking_index():
     return render_template('booking_index.html')
@@ -221,47 +308,14 @@ def booking_index():
 def charge_index():
     return render_template('charge_index.html')
 
+@app.route('/health/booking_index')
+def health_booking_index():
+    return render_template('health_booking_index.html')
+
+
 @app.route('/')
 def index():
-    (bookings_count,) = rows(g.db.execute(
-        'SELECT COUNT(rowid) FROM bookings'))
-    bookings_count = bookings_count['COUNT(rowid)']
-    now = datetime.datetime.utcnow()
-    day_0 = now.replace(hour=0, minute=0, second=0)
-    day_1 = day_0 - datetime.timedelta(days=1)
-    day_2 = day_1 - datetime.timedelta(days=1)
-    week_0 = day_0 - datetime.timedelta(days=now.weekday())
-    week_1 = week_0 - datetime.timedelta(days=7)
-    week_2 = week_1 - datetime.timedelta(days=7)
-
-    (bookings_1_count,) = rows(g.db.execute(
-        'SELECT COUNT(rowid) FROM bookings WHERE updated_on>? AND updated_on<?',
-        (day_1.strftime('%Y-%m-%d %H:%M:%S'),
-         day_0.strftime('%Y-%m-%d %H:%M:%S'))))
-    bookings_1_count = bookings_1_count['COUNT(rowid)']
-    (bookings_2_count,) = rows(g.db.execute(
-        'SELECT COUNT(rowid) FROM bookings WHERE updated_on>? AND updated_on<?',
-        (day_2.strftime('%Y-%m-%d %H:%M:%S'),
-         day_1.strftime('%Y-%m-%d %H:%M:%S'))))
-    bookings_2_count = bookings_2_count['COUNT(rowid)']
-    (bookings_w1_count,) = rows(g.db.execute(
-        'SELECT COUNT(rowid) FROM bookings WHERE updated_on>? AND updated_on<?',
-        (week_1.strftime('%Y-%m-%d %H:%M:%S'),
-         week_0.strftime('%Y-%m-%d %H:%M:%S'))))
-    bookings_w1_count = bookings_w1_count['COUNT(rowid)']
-    (bookings_w2_count,) = rows(g.db.execute(
-        'SELECT COUNT(rowid) FROM bookings WHERE updated_on>? AND updated_on<?',
-        (week_2.strftime('%Y-%m-%d %H:%M:%S'),
-         week_1.strftime('%Y-%m-%d %H:%M:%S'))))
-    bookings_w2_count = bookings_w2_count['COUNT(rowid)']
-
-    return render_template(
-        'index.html',
-        bookings_count=bookings_count,
-        bookings_1_count=bookings_1_count,
-        bookings_2_count=bookings_2_count,
-        bookings_w1_count=bookings_w1_count,
-        bookings_w2_count=bookings_w2_count)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0')
