3d91e092c307aa1129874b385b914a0a9ff762de
Karl Anderson <kra@monkey.org>
2012-10-29 07:54:04 -0700

    activate script

---
+++ scripts/activate_user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import sqlite3
+import sys
+
+from scrapy.conf import settings
+#from common import users
+
+def activate_user(conn, username):
+    conn.execute(
+        'UPDATE users SET active=1 WHERE name=?',
+        (username,))
+    conn.commit()
+
+if __name__ == '__main__':
+    try:
+        [username] = sys.argv[1:]
+    except ValueError:
+        print 'usage: deactivate_user username'
+        sys.exit(1)
+    conn = sqlite3.connect(settings['SQLITE_FILENAME'])
+
+    activate_user(conn, username)
